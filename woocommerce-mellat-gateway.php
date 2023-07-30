2d38119c6b590fa3403c65d7dcbd26c4d8dea706
ardavanshamroshan <ardavan2000.ashr@gmail.com>
2023-07-30 18:59:01 +0330

    woocommerce mellat gateway project starts

---
+++ woocommerce-mellat-gateway.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/**
+ * Plugin Name: درگاه پرداخت بانک ملت برای ووکامرس
+ * Version: 1.0
+ * Description:  این افزونه درگاه پرداخت الکترونیکی به پرداخت ملت را به افزونه فروشگاهی ووکامرس اضافه می‌کند.
+ * Plugin URI: https://themedoni.com/
+ * Author: اردوان شام روشن
+ * Author URI: https://themedoni.com/
+ * Requires at least: 5.8
+ * Requires PHP: 7.4
+ */
